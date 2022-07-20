2f8700f66e80f38da4106367ded9f16b22d4cd72
dev-delphin <eduard.zakirov.96@mail.ru>
2022-07-20 23:44:30 +0500

    btn

--- main.cpp
+++ main.cpp
@@ -8,13 +8,65 @@
  * @copyright Copyright (c) 2022
  * 
  */
+/** 
+ * @brief GTK window
+ * @image https://www.flaticon.com/ru/free-icons/ -- Autor "Flowicon"
+ * @param app 
+ * @param user_data 
+ * @return ** void
+ **/
 
+#include <gtk/gtk.h>
+#include <filesystem>
 #include <iostream>
-#include "headers/window.h"
+//#include "headers/window.h"
+
+static void print_hello (GtkWidget *widget, gpointer   data){
+  g_print ("Hello World\n");
+}
+
+static void _init_window (GtkApplication* app, gpointer user_data){
+    GtkWidget *window;
+    GdkPixbuf *pixbuf;
+      GtkWidget *button;
+  GtkWidget *box;
+    //_errorlog();
+    window = gtk_application_window_new (app);
+    gtk_window_set_title (GTK_WINDOW (window), "PLM");
+    gtk_window_set_default_size (GTK_WINDOW (window), 500, 500);
+    gtk_window_set_position (GTK_WINDOW (window), GTK_WIN_POS_CENTER);
+    pixbuf = gdk_pixbuf_new_from_file("data/img/64x64.png", NULL);
+    gtk_window_set_icon (GTK_WINDOW (window), pixbuf);
+    if (std::filesystem::exists("data/img/32x32.png")) {
+        std::cout << "exist" << std::endl;
+    } else std::cout << "not exist" << std::endl;
+    gtk_window_set_icon_name (GTK_WINDOW (window), "data/img/24x24.png");
+// btn
+  box = gtk_box_new (GTK_ORIENTATION_VERTICAL, 0);
+  gtk_widget_set_halign (box, GTK_ALIGN_CENTER);
+  gtk_widget_set_valign (box, GTK_ALIGN_CENTER);
+
+  gtk_window_set_child (GTK_WINDOW (window), box);
+
+  button = gtk_button_new_with_label ("Hello World");
+
+  g_signal_connect (button, "clicked", G_CALLBACK (print_hello), NULL);
+  g_signal_connect_swapped (button, "clicked", G_CALLBACK (gtk_window_destroy), window);
+
+  gtk_box_append (GTK_BOX (box), button);
+// end btn
+    gtk_widget_show (window);
+}
 
 int main(int argc, char **argv) {
-    _gtkwindow(argc, argv);
-   // _gtK_window(argc, argv);
-    return 0;
+    GtkApplication *app;
+    int status;
+
+    app = gtk_application_new ("org.gtk.example", G_APPLICATION_FLAGS_NONE);
+    g_signal_connect (app, "activate", G_CALLBACK (_init_window), NULL);
+    status = g_application_run (G_APPLICATION (app), argc, argv);
+    g_object_unref (app);
+
+    return status;
 }
 
--- source/window.cpp
+++ source/window.cpp
@@ -5,38 +5,38 @@
  * @param user_data 
  * @return ** void 
  */
-#include <gtk/gtk.h>
-#include <filesystem>
+// #include <gtk/gtk.h>
+// #include <filesystem>
 
 
-static void activate (GtkApplication* app, gpointer user_data){
-    GtkWidget *window;
-    GdkPixbuf *pixbuf;
-    //_errorlog();
-    window = gtk_application_window_new (app);
-    gtk_window_set_title (GTK_WINDOW (window), "PLM");
-    gtk_window_set_default_size (GTK_WINDOW (window), 500, 500);
-    gtk_window_set_position (GTK_WINDOW (window), GTK_WIN_POS_CENTER);
-    // pixbuf = gdk_pixbuf_new_from_file("../data/img/32x32.png", NULL);
-    // gtk_window_set_icon (GTK_WINDOW (window), pixbuf);
-    if (std::filesystem::exists("files.cpp")) {
-        std::cout << "exist" << std::endl;
-    } else std::cout << "not exist" << std::endl;
-    gtk_window_set_icon_name (GTK_WINDOW (window), "../data/img/24x24.png");
-    gtk_widget_show (window);
-}
+// static void activate (GtkApplication* app, gpointer user_data){
+//     GtkWidget *window;
+//     GdkPixbuf *pixbuf;
+//     //_errorlog();
+//     window = gtk_application_window_new (app);
+//     gtk_window_set_title (GTK_WINDOW (window), "PLM");
+//     gtk_window_set_default_size (GTK_WINDOW (window), 500, 500);
+//     gtk_window_set_position (GTK_WINDOW (window), GTK_WIN_POS_CENTER);
+//     // pixbuf = gdk_pixbuf_new_from_file("../data/img/32x32.png", NULL);
+//     // gtk_window_set_icon (GTK_WINDOW (window), pixbuf);
+//     if (std::filesystem::exists("files.cpp")) {
+//         std::cout << "exist" << std::endl;
+//     } else std::cout << "not exist" << std::endl;
+//     gtk_window_set_icon_name (GTK_WINDOW (window), "../data/img/24x24.png");
+//     gtk_widget_show (window);
+// }
 
-int _gtkwindow(int argc, char **argv){
-    GtkApplication *app;
-    int status;
+// int _gtkwindow(int argc, char **argv){
+//     GtkApplication *app;
+//     int status;
 
-    app = gtk_application_new ("org.gtk.example", G_APPLICATION_FLAGS_NONE);
-    g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
-    status = g_application_run (G_APPLICATION (app), argc, argv);
-    g_object_unref (app);
+//     app = gtk_application_new ("org.gtk.example", G_APPLICATION_FLAGS_NONE);
+//     g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
+//     status = g_application_run (G_APPLICATION (app), argc, argv);
+//     g_object_unref (app);
 
-    return status;
-}
+//     return status;
+// }
 /*
 static void destroy(GtkWidget *widget, gpointer data)
 {
